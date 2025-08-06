eadf37c023439796f67bf39f5060f3bffb1f6c11
Prashant Jagtap <jagtapprashant1919@gmail.com>
2025-08-07 00:28:15 +0530

    Add project files.

---
+++ Expense_Tracker/Migrations/20250806052922_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Expense_Tracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Note",
+                table: "Transactions",
+                type: "nvarchar(75)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Note",
+                table: "Transactions",
+                type: "nvarchar(10)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(75)",
+                oldNullable: true);
+        }
+    }
+}
